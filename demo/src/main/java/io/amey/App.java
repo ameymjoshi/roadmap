ef3b89f3cabbcf63fb37c26cfdc03d046b075f38
Amey Joshi <ameyjoshi@gmail.com>
2021-07-01 01:01:25 +0530

    added meth to retrive all records

--- demo/src/main/java/io/amey/App.java
+++ demo/src/main/java/io/amey/App.java
@@ -1,13 +1,12 @@
 package io.amey;
 
-import io.amey.Task.State;
-
 public class App {
     
     public static void main( String[] args )
     {
         TaskMongoDAO taskMongoDAO = new TaskMongoDAO();
-        Task newTask = new Task(1, "Buy milk", "No desc reqd", "Backlog");
-        taskMongoDAO.insertTask(newTask);
+        //Task newTask = new Task(3, "Call Sukhi", "Red pommes", "Backlog");
+        //taskMongoDAO.insertTask(newTask);
+        taskMongoDAO.getAllRecords();
     }
 }
--- demo/src/main/java/io/amey/TaskMongoDAO.java
+++ demo/src/main/java/io/amey/TaskMongoDAO.java
@@ -1,5 +1,8 @@
 package io.amey;
 
+import java.util.List;
+
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -10,17 +13,19 @@ public class TaskMongoDAO {
     
     String connURI = "mongodb://localhost:27017";
     MongoCollection<Document> collection;
-
+    
     public MongoCollection<Document> getCollection() {
         return collection;
     }
 
-    public void connect(){
+    public void connect(){        
+    
         MongoClient client = MongoClients.create(connURI);
         MongoDatabase mongoDb = client.getDatabase("TestOne");
-        collection = mongoDb.getCollection("task");        
+        collection = mongoDb.getCollection("task");
+                
     }
-
+    //Insert record in mongo
     public void insertTask(Task taskToAdd){
         connect();
         Document taskDoc = new Document("id", taskToAdd.getId())
@@ -28,5 +33,14 @@ public class TaskMongoDAO {
                                         .append("desc", taskToAdd.getDesc())
                                         .append("state", taskToAdd.getState());
         getCollection().insertOne(taskDoc);
+    }
+
+    //get all records in DB
+    public void getAllRecords(){
+        connect();
+        FindIterable<Document> taskList = collection.find();
+        for (Document document : taskList) {
+            System.out.println(document.toJson().toString());            
+        }
     }
 }
